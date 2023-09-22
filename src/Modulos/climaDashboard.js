691b7baa3ea46ce947fa512a84ede84b138e75b1
GabrielGWolf <gabrielww20@gmail.com>
2023-09-21 21:30:02 -0300

    configuraciones iniciales y prueba de funcionalidad

---
+++ src/Modulos/climaDashboard.js
@@ -0,0 +1,13 @@
+
+
+function ClimaDashboard(){
+  return (
+    <div className="clima-dashboard">Clima Dashboard
+      <h3>Clima de mi Ciuidad</h3>
+      <p>Temperatura xx °C</p>
+      <p>Humedad: xx hp </p>
+    </div>
+  );
+}
+
+export default ClimaDashboard;
---
+++ src/Modulos/transitoDashboard.js
@@ -0,0 +1,14 @@
+
+
+function transitoDashboard(){
+    return (
+      <div className="transito-dashboard">
+        <h3>Transito de mi Ciuidad</h3>
+        <p>Colectivo linea: 18 </p>
+        <p> - aca debería haber un mapa - </p>
+      </div>
+    );
+  }
+  
+  export default transitoDashboard;
+  
